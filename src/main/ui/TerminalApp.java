f78c2485369030e087c5c6e499f73d76db88b5dd
JoshD898 <jpdyce@gmail.com>
2025-02-11 09:06:26 -0800

    Added specifications for TerminalApp class

---
+++ src/main/ui/TerminalApp.java
@@ -0,0 +1,99 @@
+package ui;
+
+import model.Drawing;
+
+// An application interface that allows users to modify and interact with their gallery via the console
+public class TerminalApp {
+
+    private Drawing currentDrawing;
+
+    /*
+     * EFFECTS: creates an instance of the GalleryApp console ui application
+     */
+    public TerminalApp() {
+
+    }
+
+
+    /*
+     * MODIFIES: this
+     * EFFECTS: initializes the application with the starting values
+     */
+    public void init() {
+
+    }
+
+    /*
+     * EFFECTS: display and process inputs for the main menu
+     */
+    public void handleMainMenu() {
+
+    }
+
+    /*
+     * EFFECTS: display a list of commans that can be used in the main menu
+     */
+    public void displayMainMenu() {
+
+    }
+
+    /*
+     * EFFECTS: process the user's input in the main menu
+     */
+    public void processMainMenuInputs() {
+
+    }
+
+    /*
+     * EFFECTS: display and process input for the edit menu
+     */
+    public void handleEditMenu() {
+
+    }
+
+    /*
+     * EFFECTS: display a list of commans that can be used in the edit menu
+     */
+    public void displayEditMenu() {
+
+    }
+
+    /*
+     * EFFECTS: process the user's input in the edit menu
+     */
+    public void processEditMenuInputs() {
+
+    }
+
+    /*
+     * MODIFIES: this
+     * EFFECTS: adds a drawing to the gallery, checking that the title is unique
+     */
+    public void addDrawing() {
+
+    }
+
+    /*
+     * EFFECTS: displays string representations of every drawing in gallery
+     */
+    public void viewGallery() {
+
+    }
+
+    /*
+     * EFFECTS: displays string representations of every drawing in gallery
+     */
+    public void viewInProgressGallery() {
+
+    }
+
+    /*
+     * MODIFIES: this
+     * EFFECTS: changes currentDrawing to the drawing with the corresponding title if it exists, 
+     *          else does not modify currentDrawing
+     */
+    public void selectDrawing(String title) {
+
+    }
+
+}
